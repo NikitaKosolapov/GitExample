9a990a60c6136b5069d2db1129a99bd42bc017ce
Nikita Kosolapov <Kosolapovnikitav@gmail.com>
2020-01-16 02:46:18 +0300

    Initial Commit

---
+++ Git/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Git
+//
+//  Created by Nikita on 16/01/2020.
+//  Copyright © 2020 Nikita Kosolapov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
